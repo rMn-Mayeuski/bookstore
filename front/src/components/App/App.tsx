f3be79f26d94c1b0c95e5f1aa619d118b5915c28
Roman <100953835+rMn-Mayeuski@users.noreply.github.com>
2022-12-19 20:32:22 +0300

    split

---
+++ front/src/components/App/App.tsx
@@ -0,0 +1,12 @@
+import React, { FC } from 'react';
+import MainWrapper from '../common/MainWrapper/MainWrapper';
+
+const App: FC = () => {
+  return (
+    <MainWrapper>
+      
+    </MainWrapper>
+  );
+};
+
+export default App;
