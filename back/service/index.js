4fd9b5c4060eca85f5a1cd0f3af314cc1c290345
ViktorUserName <vityachishevich@gmail.com>
2022-12-25 22:48:39 +0300

    add Mongo Cluster, eble connect to db cloud

--- back/service/index.js
+++ back/service/index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
+const uri = 'mongodb+srv://ABChishevich:p7VzV7jCTw0fqlAv@cluster0.qiit4v7.mongodb.net/bookstore?retryWrites=true&w=majority';
+
 
 const fs = require('fs');
 const cors = require('cors');
@@ -20,7 +22,25 @@ app.use(function(request, response, next){
     response.status(404).send('that page is not find');
 });
 
-mongoose.connect('mongodb://0.0.0.0:27017/bookstore', { useUnifiedTopology: true}, function(error){
-    if(error) return console.log(error);
-    app.listen(3001);
-});
+mongoose.set('strictQuery', false);
+
+const connectDB = async () =>  {
+try {
+        await mongoose.connect(uri, { useUnifiedTopology: true}, function(error){
+        if(error){return console.log(error);}
+        app.listen(3001)
+    } );
+    console.log(`Mongo db connected`);
+} catch (error) {
+    console.log(error);
+    process.exit(1);
+}
+}
+
+connectDB();
+
+
+
+const dbConnection = mongoose.connection;
+dbConnection.on("error", (err) => console.log(`Connection error ${err}`));
+dbConnection.once("open", () => console.log("Connected to DB!"));
